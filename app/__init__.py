f49cb055d51c30f9301682c698293bccdea91a2c
achyutS1281 <achyut.shastri@gmail.com>
2023-11-04 19:38:03 -0400

    Addinng and indexing new speech_recognition modified package and adding tester functions for all classes

--- app/__init__.py
+++ app/__init__.py
@@ -7,20 +7,25 @@ import os
 import datetime
 import random
 import string
-import GPTCall as gpt
+from GPTCall import GPT
 import db
 import speechrecog as recorder
 
 app = Flask(__name__)
 app.secret_key = os.urandom(16)
-
+lang = "Spanish"
+gpt = GPT([{"role": "assistant", "content": "You are a coach helping a studnet learn a new language. Converse with "
+                                            "them in " + lang + "in 1 sentence long responses. Tell the user when "
+                                                                "they say something incorrect"}])
 
 @app.route("/", methods=['GET', 'POST'])  # At the root, we just return the homepage
 def index():
-    if request.method == 'POST':
-        caller = recorder.LangRecog()
+    if request.method:
+        caller = recorder.LangRecog("en_US")
         text = caller.listen_and_transcribe()
-        return render_template("speech.html", result = text, name=session['name'])
+
+        print(gpt.makeCall(message=text, model="gpt-4"))
+        return render_template("speech.html", result=text)
     else:
         return render_template("speech.html")
 
