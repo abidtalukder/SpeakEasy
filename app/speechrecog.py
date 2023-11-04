b5ef6807db8fa8ab08340f5a5672dc9bd96c6f61
achyutS1281 <achyut.shastri@gmail.com>
2023-11-04 16:53:44 -0400

    Updating speechRecog

--- app/speechrecog.py
+++ app/speechrecog.py
@@ -5,10 +5,13 @@ import speech_recognition as sr
 
 class LangRecog:
     def callback(self, recog, audio):
+        timeStart = time.time()
         try:
             recognized_text = recog.recognize_google(audio, language=self.language, show_all=False)
             print("You said: " + recognized_text)
             self.text = recognized_text
+            timeEnd = time.time() - timeStart
+            print(timeEnd)
         except LookupError:
             print("Unrecognized")
 
@@ -22,13 +25,16 @@ class LangRecog:
         recognizer = sr.Recognizer()
         microphone = sr.Microphone(0)
         with microphone as source:
+            recognizer.adjust_for_ambient_noise(source, 0.5)
             print("Listening for speech in other languages...")
-
-        stop_listening = recognizer.listen_in_background(source=source, callback=self.callback)
+        stop_listening = recognizer.listen_in_background(source=source, callback=self.callback, phrase_time_limit=10)
         time.sleep(4)
         # Unregister the listener and stop the recognizer
         stop_listening(wait_for_stop=True)
-        time.sleep(5)
 
     def setLanguage(self, lang):
         self.language = lang
+
+
+caller = LangRecog()
+caller.listen_and_transcribe()
