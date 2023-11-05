49c68fabbabbe1822592a6288d8e52063c63b757
Abid Talukder <atalukder30@stuy.edu>
2023-11-04 20:47:37 -0400

    merging

--- app/__init__.py
+++ app/__init__.py
@@ -1,28 +1,100 @@
-from flask import Flask  # facilitate flask webserving
-from flask import render_template  # facilitate jinja templating
+import pathlib
+from flask import Flask, abort  # facilitate flask webserving
+from flask import render_template, redirect  # facilitate jinja templating
 from flask import request  # facilitate form submission
 from flask import session
-import sqlite3
 import os
 import datetime
 import random
 import string
 import GPTCall as gpt
 import db
-import speechrecog as recorder
+
+from google.oauth2 import id_token
+from google_auth_oauthlib.flow import Flow
+from pip._vendor import cachecontrol
+import google.auth.transport.requests
+#import speechrecog as recorder
 
 app = Flask(__name__)
-app.secret_key = os.urandom(16)
+app.secret_key = "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"
 
+
+os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1" # to allow Http traffic for local dev
+
+GOOGLE_CLIENT_ID = "116824999490-2v8691iltgn318dsmeugp7gbhi21s4ha.apps.googleusercontent.com"
+client_secrets_file = os.path.join(pathlib.Path(__file__).parent, "client_secret.json")
+
+flow = Flow.from_client_secrets_file(
+    client_secrets_file=client_secrets_file,
+    scopes=["https://www.googleapis.com/auth/userinfo.profile", "https://www.googleapis.com/auth/userinfo.email", "openid"],
+    redirect_uri="http://localhost/callback"
+)
+
+
+# def login_is_required(function):
+#     def wrapper(*args, **kwargs):
+#         if "google_id" not in session:
+#             return abort(401)  # Authorization required
+#         else:
+#             return function()
+
+#     return wrapper
 
 @app.route("/", methods=['GET', 'POST'])  # At the root, we just return the homepage
 def index():
-    if request.method == 'POST':
-        caller = recorder.LangRecog()
-        text = caller.listen_and_transcribe()
-        return render_template("speech.html", result = text, name=session['name'])
-    else:
-        return render_template("speech.html")
+    return render_template("index.html")
+
+@app.route("/login")
+def login():
+    authorization_url, state = flow.authorization_url()
+    session["state"] = state
+    return redirect(authorization_url)
+
+@app.route("/callback2", methods=['GET', 'POST'])
+def callback2():
+    
+    session['name'] = "Abid Talukder"
+    session["google_id"] = "abidtalukder12@gmail.com"
+    
+    return render_template("speech.html", name=session['name'])
+
+
+@app.route("/callback")
+def callback():
+    flow.fetch_token(authorization_response=request.url)
+
+    if not session["state"] == request.args["state"]:
+        abort(500)  # State does not match!
+
+    credentials = flow.credentials
+    request_session = requests.session()
+    cached_session = cachecontrol.CacheControl(request_session)
+    token_request = google.auth.transport.requests.Request(session=cached_session)
+
+    id_info = id_token.verify_oauth2_token(
+        id_token=credentials._id_token,
+        request=token_request,
+        audience=GOOGLE_CLIENT_ID
+    )
+
+    session["google_id"] = id_info.get("sub")
+    session["name"] = id_info.get("name")
+    return redirect("/")
+
+
+# @app.route("/login", methods=['GET', 'POST'])
+# def login():
+#     if request.method == 'POST':
+#         session['name'] = request.form['name']
+#         return render_template("speech.html", name=session['name'])
+#     else:
+#         return render_template("login.html")
+    
+@app.route("/logout", methods=['GET', 'POST'])
+def logout():
+    session.clear()
+    return redirect("/")
 
 
 if __name__ == "__main__":  # false if this file imported as module
